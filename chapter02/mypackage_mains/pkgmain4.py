a05f691f3a1797d02ad911d0fcf29f8053c351c5
Lochinbek Abduvoitov <lochinbekabduvoitov8@gmail.com>
2024-04-16 09:29:46 +0500

    build pack

---
+++ chapter02/mypackage_mains/pkgmain4.py
@@ -0,0 +1,8 @@
+import sys
+
+sys.path.append('/home/cybersecurity/projects/pkb/python-for-geeks/chapter02/mypackages/masifutil')
+
+import masifutil
+
+def my_main():
+    x = masifutil
---
+++ chapter02/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='masifutilv2',
+    version='0.1.0',
+    author='Lochinbek',
+    author_email="lochinbekweb@gmail.com",
+    packages=['masifutil', 'masifutil/advcalc'],
+    python_requires='>=3.5, <4',
+    url='',
+    license='LINCENSE.txt',
+    description='A sample package for illustration purpose',
+    long_description=open('../README.md').read(),
+    install_requires=[
+
+    ],
+)
