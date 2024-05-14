127297176ba573fb8e845733fe36853f6618de1f
Lochinbek Abduvoitov <lochinbekabduvoitov8@gmail.com>
2024-05-14 08:56:24 +0500

    polimorphism

---
+++ chapter03/polimorphism.py
@@ -0,0 +1,16 @@
+class Car:
+    def __init__(self,color,seats):
+        self.i_color = color
+        self.i_seats = seats
+
+    def print_me(self, i='basic'):
+        if(i == 'basic'):
+            print(f"This car is of color {self.i_color}")
+        else:
+            print(f"This car is of color {self.i_color}, with seats {self.i_seats}")
+
+if __name__ == "__main__":
+    car = Car("blue",5)
+    car.print_me()
+    car.print_me('blah')
+    car.print_me('detail')
