a3f6f20c550cf0d44b3f34682bc34505a87f3120
Lochinbek Abduvoitov <lochinbekabduvoitov8@gmail.com>
2024-05-23 09:40:25 +0500

    ducktyping and  end of the chapter03

---
+++ chapter03/ducktyping.py
@@ -0,0 +1,16 @@
+class Car:
+    def start(self):
+        print("start engine by ignition / battery")
+
+class Cycle:
+    def start(self):
+        print("start by pushing paddles")
+
+class Horse:
+    def push(self):
+        print("start by pulling/releasing the reins")
+
+
+if __name__ == "__main__":
+    for obj in Car(),Cycle(),Horse():
+        obj.start()
