da42b2d6688f758b18179dfa05aa5756a8d24a49
Lochinbek Abduvoitov <lochinbekabduvoitov8@gmail.com>
2024-05-30 09:13:39 +0500

    binary serach

---
+++ grokking-algorithms/binary_search.py
@@ -0,0 +1,22 @@
+#binary-search
+
+def binary_search_algo(list,item):
+    low = 0
+    high = len(list)-1
+    
+    while low <= high:
+        mid = (low + high)
+        guess = list[mid]
+
+        if guess == item:
+            return mid
+        if guess > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return None 
+
+
+my_list = [1,3,5,7,9]
+
+print(binary_search_algo(my_list,3))
