83146c27e6f1ae6f3afcbc3c6037c6c2a07efc87
Lochinbek Abduvoitov <lochinbekabduvoitov8@gmail.com>
2024-05-31 10:03:55 +0500

    generator

---
+++ chapter04/generator.py
@@ -0,0 +1,10 @@
+def my_gen():
+    yield 'A'
+    yield 'B'
+    yield 'C'
+
+if(__name__ == "__main__"):
+    iter1=my_gen()
+    print(iter1.__next__())
+    print(next(iter1))
+    print(iter1.__next__())
