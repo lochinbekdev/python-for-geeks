ce039b40edd03194d6398b95d8bfe08a8b2195f9
Lochinbek Abduvoitov <lochinbekabduvoitov8@gmail.com>
2024-06-05 10:00:09 +0500

    selection sort

---
+++ grokking-algorithms/selection_sort.py
@@ -0,0 +1,8 @@
+def find_smallest(arr):
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1,len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[1]
+            smallest_index = i
+    return smallest_index
