c0af798634d88b61b1e7b86b2f7da4628f6e8b3f
Lochinbek Abduvoitov <lochinbekabduvoitov8@gmail.com>
2024-06-06 07:36:26 +0500

    recursive algorithm base case and rescursive case  and difference with loop , recursive way

---
+++ grokking-algorithms/recursion.py
@@ -0,0 +1,21 @@
+# Find with loop
+
+def look_for_key(main_box):
+    pile=main_box.make_a_pile_to_look_through()
+    while pile is not empty:
+        box = pile.grade_a_box()
+        for item in box:
+            if item.is_a_box():
+                pile.append(item)
+            else:
+                return "Key is found"
+
+
+# Recursive  algorithm
+
+def look_for_key(box):
+    for item in box:
+        if item.is_a_box():     #base case
+            look_for_key(item)  
+        else:                   #recursive case
+            return "key found"
