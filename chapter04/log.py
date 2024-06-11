1c25a20d3a875fc91affe8992ff5640a3c3f4c34
Lochinbek Abduvoitov <lochinbekabduvoitov8@gmail.com>
2024-06-11 15:23:30 +0500

    logger

---
+++ chapter04/log.py
@@ -0,0 +1,9 @@
+import logging
+
+
+logging.debug("This is a debug logging")
+logging.warning("This is a waring logging")
+logging.info("This is a info logging")
+
+
+logging.basicConfig(level=logging.DEBUG)
---
+++ chapter04/log2.py
@@ -0,0 +1,11 @@
+import logging
+
+logger1=logging.getLogger("my_logger")
+logging.basicConfig()
+logger1.setLevel(logging.INFO)
+logger1.warning("This is a warning message")
+logger1.info("This is a info message")
+logger1.debug("This is a debug logger")
+logging.info("This is a info  message")
+
+
