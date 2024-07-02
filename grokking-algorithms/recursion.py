9c28a6b1f928660f5a3a6b775d3f2f3db120f87f
Lochinbek Abduvoitov <lochinbekabduvoitov8@gmail.com>
2024-07-02 21:09:31 +0500

    working with unittest framework fist time in python programming

--- grokking-algorithms/recursion.py
+++ grokking-algorithms/recursion.py
@@ -3,7 +3,7 @@
 def look_for_key(main_box):
     pile=main_box.make_a_pile_to_look_through()
     while pile is not empty:
-        box = pile.grade_a_box()
+        box = pile.grade_a_box()midmid
         for item in box:
             if item.is_a_box():
                 pile.append(item)
